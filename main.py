7d8cc229ccb7cd71c320f03b9aef30a68246af1e
rutra_fedora <arturkummer08@gmail.com>
2024-11-12 19:59:47 +0100

    added intro

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
+import pyray as pr
 from menu import Menu
 from segregation_game import SegregationGame
 
+def show_intro():
+    pr.init_window(800, 600, "Intro")
+    pr.set_target_fps(60)
+    alpha = 255
+
+    while not pr.window_should_close() and alpha > 5:
+        pr.begin_drawing()
+        pr.clear_background(pr.GREEN)
+        text = "Help our climate!"
+        text_width = pr.measure_text(text, 40)
+        pr.draw_text(text, (pr.get_screen_width() - text_width) // 2, pr.get_screen_height() // 2, 40, pr.Color(0, 0, 0, alpha))
+        pr.end_drawing()
+
+        alpha -= 2
+
+
+    pr.close_window()
+
 def main():
+    show_intro()
     while True:
         menu = Menu()
         selected_option = menu.run()
